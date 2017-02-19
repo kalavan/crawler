342895818716b1b06d7932b05a0346bf6d618ee3
Maciej Nowakowski <nosm@viessmann-hause-automation.com>
2017-02-19 12:32:35 +0100

    Smarter looking for offert links.

--- PageSweeper.py
+++ PageSweeper.py
@@ -12,8 +12,7 @@ def loop_through_olx(start_url):
     while have_next_link:
         try:
             print "Looking for links on page %s" % browser.geturl()
-            links = find_links(browser.links())
-#            links = find_links(browser.links())
+            links = [link.url for link in browser.links(url_regex=re.compile(".*/oferta/.*"))]
             links_found = list(set(links_found + links))
             print "Trying to follow next link"
             browser.follow_link(text_regex=re.compile("^nast\xc4\x99pna"))
@@ -23,14 +22,17 @@ def loop_through_olx(start_url):
     return links_found
 
 
-def find_links(link_list):
-    links_found = []
-    for link in link_list:
-        for attr in link.attrs:
-            if attr[0] == 'class':
-                if "marginright5 link linkWithHash detailsLink" in attr[1]:
-                    links_found.append(link.url)
-    return links_found
+# def find_links(link_list):
+#     links_found = []
+#     for link in link_list:
+#         print link.url
+#
+#     for link in link_list:
+#         for attr in link.attrs:
+#             if attr[0] == 'class':
+#                 if "marginright5 link linkWithHash detailsLink" in attr[1]:
+#                     links_found.append(link.url)
+#     return links_found
 
 
 # links = loop_through_olx("https://www.olx.pl/nieruchomosci/mieszkania/?page=499")
