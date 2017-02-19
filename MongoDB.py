533d1b33fc56e4171d535cc0de2d9db8080e9227
Maciej Nowakowski <nosm@viessmann-hause-automation.com>
2017-02-19 13:40:04 +0100

    DB credentials in outside file.

--- MongoDB.py
+++ MongoDB.py
@@ -1,10 +1,11 @@
 import pymongo
+import db_access
 
-client = pymongo.MongoClient("mongodb://pblo28:test28@ds037571.mlab.com:37571/targets")
+client = pymongo.MongoClient("mongodb://%s:%s@ds037571.mlab.com:37571/targets"
+                             % (db_access.db['user'], db_access.db['password']))
 db = client.targets
-print db
 collection = db.olx
-print collection
 
-print collection.find_one({'author': 'test1'})
+for link in collection.find():
+    print link['url']
 
